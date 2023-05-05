961122e48e7a36af160e178d98f974fffb87a3a4
Nehuen Martinez <ne_martinez@farmacity.com.ar>
2023-05-04 21:17:25 -0300

    update 0.02

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, } from "react-router-dom";
-import Navbar from "./Components/Navbar/Main";
+import Navbar from "./Components/Navbar/main";
 
 function App() {
   return (
